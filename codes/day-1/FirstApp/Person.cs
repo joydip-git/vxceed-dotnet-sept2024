e7067107ea4887a52bbd2c46a8fbb73be3f0a7f8
Joydip <joydip.mondal@gmail.com>
2024-09-16 13:11:29 +0530

    day-1 update

---
+++ codes/day-1/FirstApp/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Person
+    {
+        string name;
+        long mobileNo;
+        string address;
+        string emailId;
+
+        public Person() { }
+
+        public Person(string name, long mobileNo, string address, string emailId)
+        {
+            this.name = name;
+            this.mobileNo = mobileNo;
+            this.address = address;
+            this.emailId = emailId;
+        }
+    }
+}
---
+++ codes/day-1/FirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using FirstApp;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Person joydipPerson = new Person("joydip", 9090909090, "Bangalore", "joydip@gmail.com");
+
+        Console.ReadLine();
+    }
+}
