7e1b6e3fd67fcdf57d2fc8884727cbfa69db786d
Joydip <joydip.mondal@gmail.com>
2024-09-17 13:13:44 +0530

    day-2 final update

---
+++ codes/day-2/PayrollApp/PayrollApp.Entities/Employee.cs
@@ -0,0 +1,44 @@
+﻿namespace PayrollApp.Entities
+{
+    public class Employee
+    {
+        private int id;
+        private string name;
+        private double basicPayment;
+        private double daPayment;
+        private double hraPayment;
+        private double totalSalary;
+
+        public Employee() { }
+
+        public Employee(int id, string name, double basicPayment, double daPayment, double hraPayment)
+        {
+            this.id = id;
+            this.name = name;
+            this.basicPayment = basicPayment;
+            this.daPayment = daPayment;
+            this.hraPayment = hraPayment;
+        }
+
+        public int Id { get => id; set => id = value; }
+
+        public string Name
+        { get => name; set => name = value; }
+
+        public double BasicPayment
+        { get => basicPayment; set => basicPayment = value; }
+
+        public double DaPayment
+        { get => daPayment; set => daPayment = value; }
+
+        public double HraPayment
+        { get => hraPayment; set => hraPayment = value; }
+
+        public double TotalSalary { get => totalSalary; }
+
+        public void CalculateSalary()
+        {
+            totalSalary = basicPayment + daPayment + hraPayment;
+        }
+    }
+}
---
+++ codes/day-2/PayrollApp/PayrollApp.UserInterface/Program.cs
@@ -0,0 +1,37 @@
+﻿using PayrollApp.Entities;
+
+namespace PayrollApp.UserInterface
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Employee employee = Create();
+            employee.CalculateSalary();
+            Console.WriteLine(employee.Name + " with " + employee.Id + "got " + employee.TotalSalary + " as total salary");
+        }
+
+        //method to create an employee instance with values from user
+        static Employee Create()
+        {
+            Employee employee = new Employee();
+
+            Console.Write("name: ");
+            employee.Name = Console.ReadLine();
+
+            Console.Write("id: ");
+            employee.Id = int.Parse(Console.ReadLine());
+
+            Console.Write("basic payment: ");
+            employee.BasicPayment = double.Parse(Console.ReadLine());
+
+            Console.Write("da payment: ");
+            employee.DaPayment = double.Parse(Console.ReadLine());
+
+            Console.Write("hra payment: ");
+            employee.HraPayment = double.Parse(Console.ReadLine());
+
+            return employee;
+        }
+    }
+}
