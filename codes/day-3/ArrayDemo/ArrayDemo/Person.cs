f435ace3188592bd9cac799fe637d5e3da947ef4
Joydip <joydip.mondal@gmail.com>
2024-09-18 13:21:42 +0530

    day-3 upload

---
+++ codes/day-3/ArrayDemo/ArrayDemo/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDemo
+{
+    internal class Person
+    {
+        private int id;
+        private string? name;
+
+        public Person()
+        {
+            
+        }
+
+        public Person(int id, string? name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string? Name { get => name; set => name = value; }
+
+        public object this[int index]
+        {
+            set
+            {
+                if(index == 0)
+                    id=(int)value;
+                if(index == 1)
+                    name=(string)value;
+            }
+            get
+            {
+                if (index == 0)
+                    return id;
+                else if (index == 1)
+                    return name;
+                else
+                    return "no value";
+            }
+        }
+        public object this[string key]
+        {
+            set
+            {
+                if (key == "id")
+                    id = (int)value;
+                if (key == "name")
+                    name = (string)value;
+            }
+            get
+            {
+                if (key == "id")
+                    return id;
+                else if (key == "name")
+                    return name;
+                else
+                    return "no value";
+            }
+        }
+    }
+}
---
+++ codes/day-3/ArrayDemo/ArrayDemo/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace ArrayDemo
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            /*
+            //string[] names = new string[] { "anil", "sunil", "suresh" };
+            string[] names = { "anil", "sunil", "suresh" };
+
+            Person[] people = [
+                new Person(1, "anil"),
+                new Person(2, "sunil"),
+                new Person(3, "vinod")
+            ];
+
+            int[] numbers = new int[3];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"enter value for index={i}: ");
+                numbers[i] = int.Parse(Console.ReadLine() ?? "0");
+            }
+            Console.WriteLine("\nValues\n");
+            foreach (int item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Person p = new Person();
+            //p.Id = 1;
+            //p.Name = "Test";
+            //p[0] = 1;
+            //p[1] = "Test";
+
+            //p["id"] = 1;
+            //p["name"] = "Test";
+
+            //Console.WriteLine(p["id"]);
+            //Console.WriteLine(p["name"]);
+            //Console.WriteLine(p["unknown"]);
+            */
+            int[,] twoDimArr = new int[2, 3];
+            for (int i = 0; i < twoDimArr.GetLength(0); i++)
+            {
+                for (int j = 0; j < twoDimArr.GetLength(1); j++)
+                {
+                    Console.Write($"enter value at [{i}][{j}]: ");
+                    twoDimArr[i, j] = int.Parse(Console.ReadLine() ?? "0");
+                }
+            }
+            Console.WriteLine("\nvalues\n");
+            for (int i = 0; i < twoDimArr.GetLength(0); i++)
+            {
+                for (int j = 0; j < twoDimArr.GetLength(1); j++)
+                {
+                    Console.WriteLine($"value at [{i}][{j}]: {twoDimArr[i, j]}");
+                }
+            }
+        }
+    }
+}
